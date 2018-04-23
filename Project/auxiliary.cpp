a22776d8e1a2f951bc38bea2084c8727fc32c1a6
MB <bardwell@ualberta.ca>
2018-04-23 12:49:09 -0600

    Adding feature for test

--- Project/auxiliary.cpp
+++ Project/auxiliary.cpp
@@ -9,20 +9,20 @@ double ** readfile() {
 
   string line;
   ifstream myfile ("test/trainingdata.txt");
-  // if (myfile.is_open())
-  // {
-  //     /* Inputs separated by columns (ie input 1 in data[0], 2 in data[1]) */
-  //     for (int j = 0; j < TRAININGSIZE; j++) {
-  //       for (int i = 0; i < (NOINPUTS + 1); i++) { // rows
-  //         getline(myfile, line);
-  //         data[i][j] = stod(line);
-  //         // cout << "line: " << data[i][j] << endl;
-  //         // cout << "Input [i][j] [" << i << "][" << j << "]" << endl;
-  //       }
-  //     }
-  //     return data;
-  //   myfile.close();
-  // }
-  // else cout << "Unable to open file";
+  if (myfile.is_open())
+  {
+      /* Inputs separated by columns (ie input 1 in data[0], 2 in data[1]) */
+      for (int j = 0; j < TRAININGSIZE; j++) {
+        for (int i = 0; i < (NOINPUTS + 1); i++) { // rows
+          getline(myfile, line);
+          data[i][j] = stod(line);
+          // cout << "line: " << data[i][j] << endl;
+          // cout << "Input [i][j] [" << i << "][" << j << "]" << endl;
+        }
+      }
+      return data;
+    myfile.close();
+  }
+  else cout << "Unable to open file";
   return 0;
 }
